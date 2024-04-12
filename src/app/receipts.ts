8518bc47123b374dc2181ffc9794ad60c40d863e
João Ferreira <j.pedro.ferreira@ipvc.pt>
2024-04-12 13:46:52 +0100

    add: receipts file

---
+++ src/app/receipts.ts
@@ -0,0 +1,22 @@
+export const receipts = [
+    {
+        name: 'Pão com chouriço',
+        difficulty: "fácil",
+        image: import('../assets/img/receitas/pao_com_chourico.jpg'),
+        duration: "1 hora e 30 minutos",
+        ingredients: [
+            "1 unidade de chouriço",
+            "500g de farinha com fermento",
+            "300ml de água",
+            "1 chícara de sal"
+        ],
+        preparation: [
+            "Corte o chouriço em rodelas e reserve.",
+            "Misture a farinha com o sal e adicione a água aos poucos, até obter uma massa homogénea.",
+            "Deixe a massa repousar por 30 minutos.",
+            "Estenda a massa e coloque as rodelas de chouriço.",
+            "Enrole a massa e leve ao forno por 30 minutos."
+        ],
+        history: "O pão com chouriço é uma receita tradicional portuguesa, muito apreciada em festas e romarias. Muito famosa nas feiras medievais famalicenses que ocorrem todos os anos!"
+    },
+];
